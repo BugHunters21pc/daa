71d14dacd4beafc9c2e1f55dafd3f21acac3e095
BugHunters21pc <116105892+BugHunters21pc@users.noreply.github.com>
2022-10-28 06:46:48 +0530

    Update floyd_warshell.py

--- floyd_warshell.py
+++ floyd_warshell.py
@@ -69,3 +69,36 @@ for i in range(v):
    
     if j==v-1:
         print()
+
+        
+# method 3 negative-weighted cycle
+
+V = 4
+      
+INF = 99999
+def negCyclefloydWarshall(graph):
+    dist=[[0 for i in range(V+1)]for j in range(V+1)]
+    for i in range(V):
+        for j in range(V):
+            dist[i][j] = graph[i][j]
+    for k in range(V):
+        for i in range(V):
+            for j in range(V):
+                if (dist[i][k] + dist[k][j] < dist[i][j]):
+                        dist[i][j] = dist[i][k] + dist[k][j]
+  
+    for i in range(V):
+        if (dist[i][i] < 0):
+            return True
+  
+    return False
+ 
+graph = [ [0, 1, INF, INF],
+          [INF, 0, -1, INF],
+          [INF, INF, 0, -1],
+          [-1, INF, INF, 0]]
+          
+if (negCyclefloydWarshall(graph)):
+    print("Yes")
+else:
+    print("No")
